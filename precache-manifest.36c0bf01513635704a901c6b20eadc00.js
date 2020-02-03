50c2b6de7f29f2341b5edde4992974495fab592f
KhaledHelmyMahran <khaledhelmy774@gmail.com>
2020-02-03 16:22:18 +0200

    Updates

---
+++ precache-manifest.36c0bf01513635704a901c6b20eadc00.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "572ec5fcb43bd9cd94ca3f5e5bdea7dd",
+    "url": "/ToDoList/index.html"
+  },
+  {
+    "revision": "1474f299ff7cb87cb49a",
+    "url": "/ToDoList/static/css/main.967031eb.chunk.css"
+  },
+  {
+    "revision": "e4639c813cf3a7721351",
+    "url": "/ToDoList/static/js/2.0f4fbd2b.chunk.js"
+  },
+  {
+    "revision": "7fc60fe721fa046663bd312b1d917655",
+    "url": "/ToDoList/static/js/2.0f4fbd2b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1474f299ff7cb87cb49a",
+    "url": "/ToDoList/static/js/main.e0a68b93.chunk.js"
+  },
+  {
+    "revision": "87010a7fac397e12255d",
+    "url": "/ToDoList/static/js/runtime-main.3e3783b9.js"
+  }
+]);
